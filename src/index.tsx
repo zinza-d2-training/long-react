8594263ca098716c64fd9de74e266d9b553c0262
longbd0511 <93108863+longbd0511@users.noreply.github.com>
2021-10-26 11:43:04 +0700

    Add Material UI (#10)
    * Add Material UI

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,20 @@
+import { ThemeProvider } from '@mui/material';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 import App from './App';
+import './index.css';
 import reportWebVitals from './reportWebVitals';
+import theme from './theme';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
