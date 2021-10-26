dd331336d5f54230f1b7a696e8e9e2f3f31f87fb
longbd0511 <long.bd@zinza.com.vn>
2021-10-26 17:13:30 +0700

    Đưa router vào trong App.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,6 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
-import { BrowserRouter as Router } from 'react-router-dom';
 
 import App from './App';
 import './index.css';
@@ -14,11 +13,9 @@ import theme from './theme';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
-      <ThemeProvider theme={theme}>
-        <App />
-      </ThemeProvider>
-    </Router>
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
