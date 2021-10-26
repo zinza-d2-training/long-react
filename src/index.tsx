be7bd4fe809b684aadf23f636ea3693d4f311794
longbd0511 <long.bd@zinza.com.vn>
2021-10-26 11:28:34 +0700

    add font roboto

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,10 @@
 import { ThemeProvider } from '@mui/material';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 import App from './App';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
