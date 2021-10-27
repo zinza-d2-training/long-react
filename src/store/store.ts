fb9fec68838fc752d58be131a36d698403136ee2
longbd0511 <long.bd@zinza.com.vn>
2021-10-27 10:54:11 +0700

    thêm white list cho redux persist

--- src/store/store.ts
+++ src/store/store.ts
@@ -6,14 +6,16 @@ import rootReducer from './slices';
 
 const persistConfig = {
   key: 'root',
-  storage
+  storage,
+  whitelist: []
 };
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const store = configureStore({
   reducer: persistedReducer,
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware()
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({ serializableCheck: false })
 });
 
 export const persistor = persistStore(store);
