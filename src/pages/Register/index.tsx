321235d891f10f150907c18773e93e070b97e8cd
longbd0511 <long.bd@zinza.com.vn>
2021-10-29 13:56:37 +0700

    handle submit form register

--- src/pages/Register/index.tsx
+++ src/pages/Register/index.tsx
@@ -1,8 +1,13 @@
 import { yupResolver } from '@hookform/resolvers/yup';
 import { Box, Step, StepLabel, Stepper, Typography } from '@mui/material';
+import OtpDialog from 'components/OtpDialog';
+import { useClock } from 'hooks';
 import { IRegisterForm } from 'models/register';
 import { useEffect, useState } from 'react';
 import { SubmitHandler, useForm } from 'react-hook-form';
+import { useHistory } from 'react-router';
+import { PATH_LOGIN } from 'routes';
+import { isNumberOrNull } from 'utils/validate';
 import { registerSchema } from 'validations';
 import { Step1, Step2, Step3 } from './Steps';
 
@@ -21,9 +26,9 @@ const defaultValues: IRegisterForm = {
   wards: ''
 };
 
-const Register = () => {
-  const [currentStep, setCurrentStep] = useState<number>(0);
+const START_TIME = { hours: 0, minutes: 0, seconds: 0 };
 
+const Register = () => {
   const {
     register,
     control,
@@ -39,6 +44,21 @@ const Register = () => {
     mode: 'onTouched',
     defaultValues
   });
+
+  const [currentStep, setCurrentStep] = useState<number>(0);
+  const [isOpenModal, setIsOpenModal] = useState<boolean>(false);
+  const [otp, setOtp] = useState<string>('');
+  const [isShowReSendOtp, setIsShowReSendOtp] = useState<boolean>(false);
+  const history = useHistory();
+  const { time, setTime } = useClock(START_TIME);
+
+  useEffect(() => {
+    if (time === '00:00:00') {
+      setIsShowReSendOtp(true);
+    } else {
+      setIsShowReSendOtp(false);
+    }
+  }, [time]);
 
   useEffect(() => {
     return () => {
@@ -57,8 +77,34 @@ const Register = () => {
     setCurrentStep(currentStep - 1);
   };
 
+  const handleOpenModal = () => {
+    setIsOpenModal(true);
+    setTime({ hours: 0, minutes: 2, seconds: 0 });
+  };
+
   const onSubmit: SubmitHandler<IRegisterForm> = (data) => {
     console.log(data);
+    handleOpenModal();
+  };
+
+  const handleChangeOtp = (otp: string) => {
+    if (isNumberOrNull(otp)) {
+      setOtp(otp);
+    }
+  };
+
+  const handleCloseModal = () => {
+    setOtp('');
+    setIsOpenModal(false);
+  };
+
+  const handleReSendOtp = () => {
+    setTime({ hours: 0, minutes: 2, seconds: 0 });
+  };
+
+  const handleConfirm = () => {
+    handleCloseModal();
+    history.push(PATH_LOGIN);
   };
   return (
     <Box sx={{ display: 'flex', alignItems: 'stretch', flex: 1 }}>
@@ -141,6 +187,16 @@ const Register = () => {
             </Box>
           </Box>
         </Box>
+        <OtpDialog
+          open={isOpenModal}
+          otp={otp}
+          isShowReSendOtp={isShowReSendOtp}
+          time={time}
+          onReSendOtp={handleReSendOtp}
+          onChangeOtp={handleChangeOtp}
+          onClose={handleCloseModal}
+          onConfirm={handleConfirm}
+        />
       </Box>
     </Box>
   );
