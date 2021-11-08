140d8e072255ccb3d75568e7a19e6768c7e188c2
buiduclong0511 <buiduclong0511@gmail.com>
2021-11-08 13:06:31 +0700

    sửa button download

--- src/pages/Document/index.tsx
+++ src/pages/Document/index.tsx
@@ -1,7 +1,9 @@
 import FileDownloadOutlinedIcon from '@mui/icons-material/FileDownloadOutlined';
 import {
+  Button,
   colors,
   Container,
+  Link,
   Table,
   TableBody,
   TableCell,
@@ -9,72 +11,77 @@ import {
   TableRow,
   Typography
 } from '@mui/material';
-import { PageTitle, StyledButton } from 'components';
-import { useCallback, useRef } from 'react';
+import { PageTitle } from 'components';
 import { AppLayout } from 'theme/layout';
 
 const documents = [
   {
     id: 'document_id-sdfsdf',
-    label: 'Giới thiệu nền tảng quản lý tiêm chủng vắc xin phòng Covid-19'
+    label: 'Giới thiệu nền tảng quản lý tiêm chủng vắc xin phòng Covid-19',
+    linkDocument:
+      'https://tiemchungcovid19.gov.vn/assets/portal/document/Gi%E1%BB%9Bi_thi%E1%BB%87u_n%E1%BB%81n_t%E1%BA%A3ng_qu%E1%BA%A3n_l%C3%BD_ti%C3%AAm_ch%E1%BB%A7ng_v%E1%BA%AFc_xin_ph%C3%B2ng_Covid_-_19.pdf'
   },
   {
     id: 'document_id-sdfsdfa',
     label:
-      'HD Chuẩn hóa dữ iệu và import danh sách đối tượng tiêm chủng Covid-19, danh sách nhập hồi cứu'
+      'HD Chuẩn hóa dữ iệu và import danh sách đối tượng tiêm chủng Covid-19, danh sách nhập hồi cứu',
+    linkDocument:
+      'https://tiemchungcovid19.gov.vn/assets/portal/document/Gi%E1%BB%9Bi_thi%E1%BB%87u_n%E1%BB%81n_t%E1%BA%A3ng_qu%E1%BA%A3n_l%C3%BD_ti%C3%AAm_ch%E1%BB%A7ng_v%E1%BA%AFc_xin_ph%C3%B2ng_Covid_-_19.pdf'
   },
   {
     id: 'document_id-asdasd',
-    label: 'HD cài đặt và sử dụng ứng dụng SSKĐT dành cho người dân'
+    label: 'HD cài đặt và sử dụng ứng dụng SSKĐT dành cho người dân',
+    linkDocument:
+      'https://tiemchungcovid19.gov.vn/assets/portal/document/Gi%E1%BB%9Bi_thi%E1%BB%87u_n%E1%BB%81n_t%E1%BA%A3ng_qu%E1%BA%A3n_l%C3%BD_ti%C3%AAm_ch%E1%BB%A7ng_v%E1%BA%AFc_xin_ph%C3%B2ng_Covid_-_19.pdf'
   },
   {
     id: 'document_id-qweqwe',
     label:
-      'HD phê duyệt và phân bố yêu cầu đăng ký tiêm chủng vắc xin phòng Covid-19'
+      'HD phê duyệt và phân bố yêu cầu đăng ký tiêm chủng vắc xin phòng Covid-19',
+    linkDocument:
+      'https://tiemchungcovid19.gov.vn/assets/portal/document/Gi%E1%BB%9Bi_thi%E1%BB%87u_n%E1%BB%81n_t%E1%BA%A3ng_qu%E1%BA%A3n_l%C3%BD_ti%C3%AAm_ch%E1%BB%A7ng_v%E1%BA%AFc_xin_ph%C3%B2ng_Covid_-_19.pdf'
   },
   {
     id: 'document_id-ghjgh',
-    label: 'HD đăng ký cơ sở tiêm chủng Covid-19'
+    label: 'HD đăng ký cơ sở tiêm chủng Covid-19',
+    linkDocument:
+      'https://tiemchungcovid19.gov.vn/assets/portal/document/Gi%E1%BB%9Bi_thi%E1%BB%87u_n%E1%BB%81n_t%E1%BA%A3ng_qu%E1%BA%A3n_l%C3%BD_ti%C3%AAm_ch%E1%BB%A7ng_v%E1%BA%AFc_xin_ph%C3%B2ng_Covid_-_19.pdf'
   },
   {
     id: 'document_id-fdbdfb',
-    label: 'HD đăng ký tiêm chủng Covid-19'
+    label: 'HD đăng ký tiêm chủng Covid-19',
+    linkDocument:
+      'https://tiemchungcovid19.gov.vn/assets/portal/document/Gi%E1%BB%9Bi_thi%E1%BB%87u_n%E1%BB%81n_t%E1%BA%A3ng_qu%E1%BA%A3n_l%C3%BD_ti%C3%AAm_ch%E1%BB%A7ng_v%E1%BA%AFc_xin_ph%C3%B2ng_Covid_-_19.pdf'
   },
   {
     id: 'document_id-werer',
-    label: 'HD đăng ký tiêm chủng Covid-19 dành cho cơ quan, tổ chức'
+    label: 'HD đăng ký tiêm chủng Covid-19 dành cho cơ quan, tổ chức',
+    linkDocument:
+      'https://tiemchungcovid19.gov.vn/assets/portal/document/Gi%E1%BB%9Bi_thi%E1%BB%87u_n%E1%BB%81n_t%E1%BA%A3ng_qu%E1%BA%A3n_l%C3%BD_ti%C3%AAm_ch%E1%BB%A7ng_v%E1%BA%AFc_xin_ph%C3%B2ng_Covid_-_19.pdf'
   },
   {
     id: 'document_id-axsxz',
-    label: 'HD đăng ký tiêm chủng Covid-19 dành cho người dân'
+    label: 'HD đăng ký tiêm chủng Covid-19 dành cho người dân',
+    linkDocument:
+      'https://tiemchungcovid19.gov.vn/assets/portal/document/Gi%E1%BB%9Bi_thi%E1%BB%87u_n%E1%BB%81n_t%E1%BA%A3ng_qu%E1%BA%A3n_l%C3%BD_ti%C3%AAm_ch%E1%BB%A7ng_v%E1%BA%AFc_xin_ph%C3%B2ng_Covid_-_19.pdf'
   },
   {
     id: 'document_id-dfgdfg',
-    label: 'HDSD ứng dụng SSKĐT trong quá trình tiêm chủng Covid-19'
+    label: 'HDSD ứng dụng SSKĐT trong quá trình tiêm chủng Covid-19',
+    linkDocument:
+      'https://tiemchungcovid19.gov.vn/assets/portal/document/Gi%E1%BB%9Bi_thi%E1%BB%87u_n%E1%BB%81n_t%E1%BA%A3ng_qu%E1%BA%A3n_l%C3%BD_ti%C3%AAm_ch%E1%BB%A7ng_v%E1%BA%AFc_xin_ph%C3%B2ng_Covid_-_19.pdf'
   },
   {
     id: 'document_id-mbnmb',
-    label: 'Quy trình ứng dụng nền tảng quản lý điều hành tiêm chủng Covid-19'
+    label: 'Quy trình ứng dụng nền tảng quản lý điều hành tiêm chủng Covid-19',
+    linkDocument:
+      'https://tiemchungcovid19.gov.vn/assets/portal/document/Gi%E1%BB%9Bi_thi%E1%BB%87u_n%E1%BB%81n_t%E1%BA%A3ng_qu%E1%BA%A3n_l%C3%BD_ti%C3%AAm_ch%E1%BB%A7ng_v%E1%BA%AFc_xin_ph%C3%B2ng_Covid_-_19.pdf'
   }
 ];
 
 export const Document = () => {
-  const linkRef = useRef<HTMLAnchorElement | null>(null);
-  const handleDownload = useCallback(() => {
-    if (linkRef.current) {
-      linkRef.current.click();
-    }
-  }, []);
-
   return (
     <AppLayout>
-      <a
-        ref={linkRef}
-        href="no_link"
-        download="document.txt"
-        style={{ display: 'none' }}>
-        link
-      </a>
       <PageTitle>Tài liệu</PageTitle>
       <Container maxWidth="xl">
         <Table>
@@ -111,11 +118,13 @@ export const Document = () => {
                   <Typography variant="body1">{document.label}</Typography>
                 </TableCell>
                 <TableCell sx={{ py: 1 }} align="center">
-                  <StyledButton
-                    startIcon={<FileDownloadOutlinedIcon />}
-                    onClick={handleDownload}>
+                  <Button
+                    component={Link}
+                    href={document.linkDocument}
+                    target="_blank"
+                    startIcon={<FileDownloadOutlinedIcon />}>
                     Download
-                  </StyledButton>
+                  </Button>
                 </TableCell>
               </TableRow>
             ))}
